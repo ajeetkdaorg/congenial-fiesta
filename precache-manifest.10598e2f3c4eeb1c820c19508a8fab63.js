57cbd6081f90acec7d7de17e8d0a977195678f6a
codejeet <ajeetkokatay@gmail.com>
2020-12-15 20:53:14 -0800

    t

---
+++ precache-manifest.10598e2f3c4eeb1c820c19508a8fab63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199bc763ceeb792a8ff4980aa8ad9f2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ccbba9a65e57e48603e",
+    "url": "/static/css/main.dc472e6a.chunk.css"
+  },
+  {
+    "revision": "501841d76bb0cd9e8e1e",
+    "url": "/static/js/2.4ac92045.chunk.js"
+  },
+  {
+    "revision": "acbe28a37d0a630e99f198ce1d5a67c7",
+    "url": "/static/js/2.4ac92045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ccbba9a65e57e48603e",
+    "url": "/static/js/main.4a49bac1.chunk.js"
+  },
+  {
+    "revision": "3b5323a588e31979ecca",
+    "url": "/static/js/runtime-main.393d0999.js"
+  }
+]);
